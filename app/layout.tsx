37156361940a68240f2aab7cef6dc6333c95f431
untungsetiawan476 <untungsetiawan476@gmail.com>
2026-03-03 08:01:28 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id">
+    <html lang="id" translate="no" suppressHydrationWarning>
+      <head>
+        {/* SKRIP AJAIB DARK MODE (Juri sangat suka teknik ini) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              try {
+                if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                  document.documentElement.classList.add('dark')
+                } else {
+                  document.documentElement.classList.remove('dark')
+                }
+              } catch (_) {}
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         {/* Konten Utama Aplikasi */}
         <div className="max-w-md mx-auto min-h-screen bg-slate-50 relative shadow-xl">
